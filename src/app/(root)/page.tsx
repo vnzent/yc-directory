6fcd9ba1cb422b9cc613c0564277d979920dc97f
Vincent <vincent.ptk17@gmail.com>
2025-10-17 10:57:41 +0800

    implement heading at Home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-3xl">Home</h1>
-    </>
+    <section className="w-full bg-primary min-h-[530px] flex flex-col justify-center items-center py-10 px-6 pattern">
+      <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white text-[36px] leading-[46px] sm:text-[54px] sm:leading-[64px] max-w-5xl text-center my-5">Pitch Your Startup, <br/>Connect With Entrepreneurs</h1>
+      <p className="font-medium text-[20px] text-white text-center break-words">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+    </section>
   );
 }
